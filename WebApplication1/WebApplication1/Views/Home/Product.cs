4aaae1a51ac8a8cc45877a6e565dcd70addfc4b5
Hatr727 <hatr626@gmail.com>
2024-11-06 16:11:21 -0600

    33s

---
+++ WebApplication1/WebApplication1/Views/Home/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication1.Views.Home
+{
+    public class Product
+    {
+        public string name;
+        public double price;
+
+        public Product(string name, double price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
